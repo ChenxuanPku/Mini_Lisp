66c3e322087f1c3477acc502eb08c836c5497c91
ChenxuanPku <chenxuan@stu.pku.edu.cn>
2024-05-14 07:36:26 +0800

    Update parser.cpp

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,24 +5,34 @@ ValuePtr Parser::parse()
 {
     if (tokens.empty()) throw SyntaxError("Empty");
     auto token=std::move(tokens.front());//智能指针不能复制 
+    if(token->getType()==TokenType::IDENTIFIER)
+    {
+        auto value = static_cast<IdentifierToken&>(*token).getName();
+        tokens.pop_front();
+        return std::make_shared<SymbolValue>(value);
+    }
     if(token->getType()==TokenType::NUMERIC_LITERAL)
     {
          auto value = static_cast<NumericLiteralToken&>(*token).getValue();
+         tokens.pop_front();
         return std::make_shared<NumericValue>(value);
     }
     else
      if(token->getType()==TokenType::BOOLEAN_LITERAL)
     {
          auto value = static_cast<BooleanLiteralToken&>(*token).getValue();
+         tokens.pop_front();
         return std::make_shared<BooleanValue>(value);
     }
     else if
     (token->getType()==TokenType::STRING_LITERAL)
     {
         auto value = static_cast<StringLiteralToken&>(*token).getValue();
+        tokens.pop_front();
         return std::make_shared<StringValue>(value);
     }
     else if(token->toString()=="(LEFT_PAREN)"){
+        tokens.pop_front();
         return this->parseTails();
     }
     else if(token->getType()==TokenType::QUOTE){
@@ -61,7 +71,7 @@ ValuePtr Parser::parse()
 }
 
 ValuePtr Parser::parseTails(){
-    tokens.pop_front();
+   
     if (tokens.empty()) throw SyntaxError("Empty1");
     if (tokens.front()->toString()=="(RIGHT_PAREN)")
     {
@@ -69,7 +79,7 @@ ValuePtr Parser::parseTails(){
         return std::make_shared<NilValue>();
     }
     auto car=this->parse();
-    tokens.pop_front();
+    
     if (tokens.empty()) throw SyntaxError("Empty2");
     if (tokens.front()->toString()=="(DOT)")
     {
@@ -82,13 +92,13 @@ ValuePtr Parser::parseTails(){
        // if (tokens.front()->toString()!="(RIGHT_PAREN)")
        // 这里理应有检测点，但不知道为什么写不过去
        
-            tokens.pop_front();
+            
             return std::make_shared<PairValue>(car,cdr);
         
        // else throw SyntaxError("Not_Rightparen");
     }
     else{
-        auto cdr=this->parse();
+        auto cdr=this->parseTails();
         return std::make_shared<PairValue>(car,cdr);
     }
 
